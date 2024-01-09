2b103cf0c1d898e89c041ef762fc35313b98a350
Thomas Rice <ricethomasj@gmail.com>
2024-01-08 21:46:22 -0600

    Project setup. Auth configured.

--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -2,12 +2,18 @@ import PropTypes from 'prop-types';
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+// import { Row, Col } from 'react-bootstrap';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+// import { Col } from 'react-bootstrap'
 import { registerUser } from '../utils/auth'; // Update with path to registerUser
 
 function RegisterForm({ user, updateUser }) {
   const [formData, setFormData] = useState({
-    bio: '',
+    first_name: '',
     uid: user.uid,
+    last_name: '',
+    is_admin: true,
   });
 
   const handleSubmit = (e) => {
@@ -17,17 +23,30 @@ function RegisterForm({ user, updateUser }) {
 
   return (
     <Form onSubmit={handleSubmit}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Gamer Bio</Form.Label>
-        <Form.Control as="textarea" name="bio" required placeholder="Enter your Bio" onChange={({ target }) => setFormData((prev) => ({ ...prev, [target.name]: target.value }))} />
-        <Form.Text className="text-muted">Let other gamers know a little bit about you...</Form.Text>
-      </Form.Group>
+      <Row className="mb-3">
+        <Form.Group className="mb-3" controlId="formBasicEmail" as={Col}>
+          <Form.Label>First Name</Form.Label>
+          <Form.Control name="first_name" required placeholder="Enter your first name" onChange={({ target }) => setFormData((prev) => ({ ...prev, [target.name]: target.value }))} />
+        </Form.Group>
+
+        <Form.Group className="mb-3" as={Col}>
+          <Form.Label>Last Name</Form.Label>
+          <Form.Control
+            name="last_name"
+            required
+            placeholder="Enter your last name"
+            onChange={({ target }) => setFormData((prev) => ({ ...prev, [target.name]: target.value }))}
+          />
+        </Form.Group>
+      </Row>
       <Button variant="primary" type="submit">
         Submit
       </Button>
     </Form>
   );
 }
+
+// onChange={({ target }) => setFormData((prev) => ({ ...prev, [target.first_name]: target.value }))}
 
 RegisterForm.propTypes = {
   user: PropTypes.shape({
